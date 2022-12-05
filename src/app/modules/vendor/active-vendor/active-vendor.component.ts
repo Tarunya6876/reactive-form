529bffa41da17d4bf34ee2bb1e1d13e6a4a4ff7b
Gundlapalli Varalakshmi <gundlapalli.v@accolite.com>
2022-12-05 22:44:40 +0530

    Done

--- src/app/modules/vendor/active-vendor/active-vendor.component.ts
+++ src/app/modules/vendor/active-vendor/active-vendor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import{FormControl, FormGroup} from '@angular/forms';
+import{FormBuilder,Validators} from '@angular/forms';
 @Component({
   selector: 'app-active-vendor',
   templateUrl: './active-vendor.component.html',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActiveVendorComponent implements OnInit {
 
-  constructor() { }
+  //public form:FormGroup;
+registerForm= new FormGroup({
+  name:new FormControl('',Validators.required),
+  email:new FormControl('',Validators.required),
+  bio:new FormControl('',Validators.required),
+  //la:new FormControl('',Validators.required)
+
+});
+get name()
+{
+  return this.registerForm.get('name');
+}
+get email()
+{
+  return this.registerForm.get('email');
+}
+get bio()
+{
+  return this.registerForm.get('bio');
+}
+
+title='validation';
+submitted=false;
+
+constructor() { }
+
+
 
   ngOnInit(): void {
-  }
 
+  }
+  myVal!:any;
+  onSubmit(){
+    this.myVal=this.registerForm.value;
+    this.submitted=true;
+    if(this.registerForm.invalid){
+    return
+    
+  }
+  alert("Success");
+  //alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+
+}
 }
